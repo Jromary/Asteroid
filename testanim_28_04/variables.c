88ee34d5e679a975bfb08b19a9907a1eb943ac1e
eperrindeckert <manuper1deckert@hotmail.fr>
2017-04-28 09:48:13 +0200

    Add files via upload

---
+++ testanim_28_04/variables.c
@@ -0,0 +1,9 @@
+#include "variables.h"
+
+int gameover = 0;
+Sprite asteroidTab[ASTEROID_MAX];
+int nbAsteroid = 0;
+Sprite shootTab[SHOOT_MAX];
+int nbShoot = 0;
+Sprite animTab[ANIM_MAX];
+int nbAnim = 0;
---
+++ testanim_28_04/variables.h
@@ -0,0 +1,49 @@
+#ifndef VARIABLES_H
+#define VARIABLES_H
+
+#define screen_height 480
+#define screen_length 640
+
+#define PI 3.1416
+
+#define COEFF_FROTTEMENT 0.001
+#define COEFF_BOOST 0.01
+
+#define ASTEROID_MAX 1000
+
+#define size_asteroid_min 16
+#define size_asteroid_max 64
+#define size_asteroid_med 32
+#define ship_size 32
+#define ship_angle 0
+
+#define bullet_size 8
+#define SHOOT_MAX 1
+
+#define ANIM_MAX 10
+
+
+#include <SDL.h>
+
+struct Pointreel
+{
+	float rx;
+	float ry;
+};
+typedef struct Pointreel Pointreel;
+
+struct Sprite
+{
+	Pointreel pr;
+	SDL_Rect pi;
+	int angle;
+	Pointreel accel;
+	Pointreel speed;
+	SDL_Surface image;
+	int spritetaille;
+};
+typedef struct Sprite Sprite;
+
+
+
+#endif /* VARIABLES_H */
