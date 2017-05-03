826edf1c01b6f6b5eb4b494a8bd3a9bb3d68f8b8
eperrindeckert <manuper1deckert@hotmail.fr>
2017-05-03 11:41:35 +0200

    Gestion evenement multitouches
    Nouvelle fonction pour multijoueurs / multitouches
    Collision approximative entre le vaisseau et les asteroïdes

---
+++ multitouches_03_05/variables.c
@@ -0,0 +1,10 @@
+#include "variables.h"
+
+int gameover = 0;
+Sprite asteroidTab[ASTEROID_MAX];
+int nbAsteroid = 0;
+Sprite shootTab[SHOOT_MAX];
+int nbShoot = 0;
+Sprite animTab[ANIM_MAX];
+int nbAnim = 0;
+int shoot_again = 1;
